ec1ef6912d472d18570a5f6bca7873be9ca7e825
lakustovdimka <lakustovdimka@gmail.com>
2025-08-09 22:18:51 +0500

    12

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    # Use a breakp1oint in the code line below to debug your script.
+    # Use a breakp12oint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
